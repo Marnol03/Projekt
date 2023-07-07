3e7a72bbf19f7175c0379dccea62116e56bc4766
FRAID <wfomekong0@gmail.com>
2023-07-07 17:28:32 +0200

    lezteversion0

--- warenkorb.js
+++ warenkorb.js
@@ -103,11 +103,13 @@ else{
       mastercard.style.display='none';
     visa.style.display='none';
     error.style.display='block'
-
+ 
 }
 }
 
+kartenummerinput.addEventListener('input', oncardNumberChange);
 
+//
 function isvalidcardNumber (cardNumber) {
     const VALID_REGEX = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
     return VALID_REGEX.test(cardNumber)
